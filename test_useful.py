b7059452861bc4d219981adb02dc6f85cf3838d9
galla-okto <galla.okto1@gmail.com>
2020-12-06 14:17:40 +0200

    Добавляет тесты функций из файла useful

--- test_useful.py
+++ test_useful.py
@@ -5,12 +5,20 @@ class TestUsefulGenerateNumberNotUsed:
         res = generate_number_not_used(list_numbers90, list_numbers_used)
         assert res in range(1, 21)
 
+    def test_return_type_int(self, list_numbers90, list_numbers_used):
+        res = generate_number_not_used(list_numbers90, list_numbers_used)
+        assert isinstance(res, int)
+
 class TestUsefulFindFreePlace:
     def test_return_index(self, list_numbers10):
         res = find_free_place(list_numbers10)
         assert res in ([index + 1 for index in range(len(list_numbers10)) if list_numbers10[index] == 0])
 
+    def test_return_type_int(self, list_numbers10):
+        res = find_free_place(list_numbers10)
+        assert isinstance(res, int)
+
 class TestFillListNumbers:
-    def test_fill_list_numbers(self, list_numbers90, list_numbers_used):
+    def test_return_list_numbers(self, list_numbers90, list_numbers_used):
         res = fill_list_numbers(list_numbers90, list_numbers_used)
-        assert res
+        assert len([item for item in res if item != 0]) == 5
