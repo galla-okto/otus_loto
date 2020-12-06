9b4307b53a99e3d08f93756451fe4597e57e69b9
galla-okto <galla.okto1@gmail.com>
2020-12-06 15:05:13 +0200

    Добавляет еще один тест для методов из класса Card

--- test_card.py
+++ test_card.py
@@ -20,6 +20,7 @@ class TestCardSearchNumber:
         res = card_tmp.search_number(number_for_search)
         assert res == True
 
+
 class TestCardCheckCard:
     def test_return_check_card(self):
         cards_numbers = [item for item in range(1, 16)]
@@ -28,3 +29,13 @@ class TestCardCheckCard:
         card_tmp = Card(cards_numbers, cards_numbers_used)
         res = card_tmp.check_card()
         assert res == False
+
+class TestCardSearchAndDeleteNumber:
+    def test_return_search_and_delete_number(self):
+        number_for_search = 3
+        cards_numbers = [item for item in range(1, 16)]
+        cards_numbers_used = []
+
+        card_tmp = Card(cards_numbers, cards_numbers_used)
+        res = card_tmp.search_and_delete_number(number_for_search)
+        assert res == True
